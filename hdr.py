6433c484c44d7add4c0f98be054f52d4a01c2633
Pamphile ROY <roy@cerfacs.fr>
2017-07-28 17:11:38 +0200

    Add extra_alpha if other than defaults and plot to file option

--- hdr.py
+++ hdr.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import openturns as ot
 from sklearn.decomposition import PCA
@@ -5,7 +6,8 @@ import matplotlib.pyplot as plt
 plt.switch_backend('Qt5Agg')
 
 
-def hdr_boxplot(data, alpha=[], threshold=0.9, n_contours=50, plot_data=False):
+def hdr_boxplot(data, x_common=None, path=None, alpha=[], threshold=0.9, n_contours=50,
+    plot_data=False, xlabel='t', ylabel='y'):
     """High Density Region boxplot.
 
     Using the dataset :attr:`data`:
@@ -16,10 +18,13 @@ def hdr_boxplot(data, alpha=[], threshold=0.9, n_contours=50, plot_data=False):
     4. Compute mediane curve along with quartiles and outliers.
 
     :param np.array data: dataset (n_samples, n_features)
+    :param list(float) x_common: abscissa
     :param list(float) alpha: extra contour values
     :param float threshold: threshold for outliers
     :param int n_contours: discretization to compute contour
     :param bool plot_data: append data on the bivariate plot
+    :param str xlabel: label for x axis
+    :param str ylabel: label for y axis
     :returns: mediane curve along with 50%, 90% quartile (inf and sup curves) and outliers.
     :rtypes: np.array, list(np.array), np.array
     """
@@ -30,12 +35,6 @@ def hdr_boxplot(data, alpha=[], threshold=0.9, n_contours=50, plot_data=False):
 
     print('Explained variance ratio (first two components): {}'
           .format(pca.explained_variance_ratio_))
-
-    plt.figure('Bivariate space')
-    plt.scatter(data_r[:, 0], data_r[:, 1], alpha=.8)
-    plt.xlabel('First component')
-    plt.ylabel('Second component')
-    plt.show()
 
     # Create gaussian kernel
     kernel = ot.KernelSmoothing()
@@ -51,53 +50,25 @@ def hdr_boxplot(data, alpha=[], threshold=0.9, n_contours=50, plot_data=False):
     contour_stack = np.dstack(contour_grid).reshape(-1, 2)
 
     pdf = ks_gaussian.computePDF(contour_stack)
-    pdf = np.array(pdf).reshape((n_contours, n_contours))
-
-    X1, X2 = contour_grid
+    pdf = np.array(pdf).flatten()
 
     # Compute contour line of pvalue linked to a given probability level
-    alpha.extend([threshold, 0.9, 0.5, 0.001])
+    alpha.extend([threshold, 0.9, 0.5])  # 0.001
     alpha = list(set(alpha))
     alpha.sort(reverse=True)
     print('alpha: ', alpha)
 
     n_contour_lines = len(alpha)
-    pvalues = np.zeros(n_contour_lines)
+    pvalues = []
     for i in range(n_contour_lines):
         _, pvalue = ks_gaussian.computeMinimumVolumeLevelSetWithThreshold(alpha[i])
-        pvalues[i] = pvalue
+        pvalues.append(pvalue)
 
     print('pvalues: ', pvalues)
 
-    # Create contour plot
-    plt.figure('Bivariate space: 2D Kernel Smoothing with Gaussian kernel')
-    fig = plt.contour(*contour_grid, pdf, pvalues)
-
-    # Labels: probability instead of density
-    fmt = {}
-    for i in range(n_contour_lines):
-        l = fig.levels[i]
-        fmt[l] = "%.0f %%" % (alpha[i] * 100)
-    plt.clabel(fig, fig.levels, inline=True, fontsize=10, fmt=fmt)
-
-    if plot_data:
-        plt.plot(data_r[:, 0], data_r[:, 1], "b.")
-    plt.xlabel('First component')
-    plt.ylabel('Second component')
-
-    median = fig.collections[alpha.index(0.001)].get_paths()[0]
-    median = np.median(median.vertices, axis=0)
-    plt.plot(median[0], median[1], c='r', marker='^')
-
-    plt.show()
-
     # Find mean, quartiles and outliers curves
-    pdf = pdf.flatten()
-
-    # median = pdf.argmax()
-    # median = contour_stack[median]
-    # plt.plot(median[0], median[1], c='r', marker='^')
-    # plt.show()
+    median_r = pdf.argmax()
+    median_r = contour_stack[median_r]
 
     pdf_data = np.array(ks_gaussian.computePDF(data_r)).flatten()
     outliers = np.where(pdf_data < pvalues[alpha.index(threshold)])
@@ -109,8 +80,20 @@ def hdr_boxplot(data, alpha=[], threshold=0.9, n_contours=50, plot_data=False):
     mean_quartile = np.where(pdf > pvalues[alpha.index(0.5)])
     mean_quartile = contour_stack[mean_quartile]
 
+    extra_alpha = [i for i in alpha if 0.5 != i and 0.9 != i and threshold != i]
+    if extra_alpha != []:
+        extra_quartiles = []
+        for i in extra_alpha:
+            extra_quartile = np.where(pdf > pvalues[alpha.index(i)])
+            extra_quartile = contour_stack[extra_quartile]
+            extra_quartile = pca.inverse_transform(extra_quartile)
+            extra_quartiles.extend([extra_quartile.max(axis=0),
+                                    extra_quartile.min(axis=0)])
+    else:
+        extra_quartiles = None
+
     # Inverse transform from bivariate plot to dataset
-    median = pca.inverse_transform(median)
+    median = pca.inverse_transform(median_r)
     outliers = pca.inverse_transform(outliers)
     extreme_quartile = pca.inverse_transform(extreme_quartile)
     mean_quartile = pca.inverse_transform(mean_quartile)
@@ -118,29 +101,53 @@ def hdr_boxplot(data, alpha=[], threshold=0.9, n_contours=50, plot_data=False):
     extreme_quartile = [extreme_quartile.max(axis=0), extreme_quartile.min(axis=0)]
     mean_quartile = [mean_quartile.max(axis=0), mean_quartile.min(axis=0)]
 
-    plt.figure('Time Serie')
-    x_common = np.linspace(1, 12, dim)
-    plt.plot(np.array([x_common] * n_sample).T, data.T, alpha=.2)
+    # Plots
+    plt.figure('Bivariate space')
+    plt.scatter(data_r[:, 0], data_r[:, 1], alpha=.8)
+    plt.xlabel('First component')
+    plt.ylabel('Second component')
 
+    plt.figure('Bivariate space: 2D Kernel Smoothing with Gaussian kernel')
+    fig = plt.contour(*contour_grid, pdf.reshape((n_contours, n_contours)), pvalues)
+    # Labels: probability instead of density
+    fmt = {}
+    for i in range(n_contour_lines):
+        l = fig.levels[i]
+        fmt[l] = "%.0f %%" % (alpha[i] * 100)
+    plt.clabel(fig, fig.levels, inline=True, fontsize=10, fmt=fmt)
+
+    if plot_data:
+        plt.plot(data_r[:, 0], data_r[:, 1], "b.")
+    plt.xlabel('First component')
+    plt.ylabel('Second component')
+    # median = fig.collections[alpha.index(0.001)].get_paths()[0]
+    # median = np.median(median.vertices, axis=0)
+    plt.plot(*median_r, c='r', marker='^')
+
+    plt.figure('Time Serie')
+    if x_common is None:
+        x_common = np.linspace(0, 1, dim)
+    plt.plot(np.array([x_common] * n_sample).T, data.T, alpha=.2)
     plt.fill_between(x_common, *mean_quartile, color='gray', alpha=.4)
     plt.fill_between(x_common, *extreme_quartile, color='gray', alpha=.4)
 
+    try:
+        plt.plot(np.array([x_common] * len(extra_quartiles)).T, np.array(extra_quartiles).T, color='c', ls='-.', alpha=.4)
+    except TypeError:
+        pass
+
     plt.plot(x_common, median, c='k')
     plt.plot(np.array([x_common] * len(outliers)).T, outliers.T, c='r', alpha=0.7)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
 
-    plt.xlabel('Month of the year')
-    plt.ylabel('Water surface temperature (°C)')
-    plt.show()
+    try:
+        path = os.path.join(path, 'hdr_boxplot.pdf')
+        fig.tight_layout()
+        fig.savefig(path, transparent=True, bbox_inches='tight')
+    except TypeError:
+        plt.show()
+    
+    plt.close('all')
 
-    return median, outliers, extreme_quartile, mean_quartile
-
-
-# Water surface temperature data from:
-# https://www.math.univ-toulouse.fr/~ferraty/SOFTWARES/NPFDA/npfda-datasets.html
-# http://www.cpc.ncep.noaa.gov/data/indices/
-data = np.loadtxt('data/elnino.dat')
-print('Data shape: ', data.shape)
-
-output = hdr_boxplot(data)
-output = hdr_boxplot(data, alpha=[0.8])
-output = hdr_boxplot(data, alpha=[0.8], threshold=0.7)
+    return median, outliers, extreme_quartile, mean_quartile, extra_quartiles
